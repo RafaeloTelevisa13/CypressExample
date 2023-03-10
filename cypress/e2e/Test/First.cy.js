e9465d7c0bde025750c07f4f5d525a66351320cc
RafaeloTelevisa13 <cervantesromerorrr@gmail.com>
2023-03-10 17:27:05 -0600

    Add all productos in the shoppin cart

--- cypress/e2e/Test/First.cy.js
+++ cypress/e2e/Test/First.cy.js
@@ -1,32 +1,32 @@
-let add = (a,b) => a+b;
-let substract = (a,b) => a-b;
-let divide = (a,b) => a/b;
-let multiply = (a,b) => a*b;
+let add = (a, b) => a + b;
+let substract = (a, b) => a - b;
+let divide = (a, b) => a / b;
+let multiply = (a, b) => a * b;
 
-describe ('Unit testing for our dummy aplication', () => {
+describe('Unit testing for our dummy aplication', () => {
     context('Math whit positive numbers', () => {
         it('should add positive numbers', () => {
-            expect(add(1,2)).to.eq(3)
+            expect(add(1, 2)).to.eq(3)
         })
         it('should substract positive numbers', () => {
-            expect(substract(4,2)).to.eq(2)
+            expect(substract(4, 2)).to.eq(2)
         })
         it('should divide positive numbers', () => {
-            expect(divide(4,2)).to.eq(2)
+            expect(divide(4, 2)).to.eq(2)
         })
         it('should multiply positive numbers', () => {
-            expect(multiply(4,2)).to.eq(8)
+            expect(multiply(4, 2)).to.eq(8)
         })
     });
 
     context('Math whit decimal numbers', () => {
         it('should add decimal numbers', () => {
-            expect(add(4.2,2.2)).to.eq(6.4)
+            expect(add(4.2, 2.2)).to.eq(6.4)
         })
         it('should substract decimal numbers', () => {
-            expect(substract(4.4,2.2)).to.eq(2.2)
+            expect(substract(4.4, 2.2)).to.eq(2.2)
 
         })
-       
+
     });
 })
--- cypress/e2e/Test/SawgLabs.cy.js
+++ cypress/e2e/Test/SawgLabs.cy.js
@@ -7,25 +7,23 @@ describe('Send deliver', () => {
 
 
     it('Complete inputs', () => {
-        cy.addProduct();
+        cy.addOneProduct();
+        cy.get('.shopping_cart_link').click();
+        cy.get('[data-test="checkout"]').click();
         cy.get('[data-test="firstName"]').type(loginDates.nameUser);
         cy.get('[data-test="lastName"]').type(loginDates.lastName);
         cy.get('[data-test="postalCode"]').type(loginDates.CP);
         cy.get('[data-test="continue"]').click()
         cy.scrollDownUp()
-
-        cy.get('.summary_subtotal_label').then(function ($elem) {
-
-            const priceProduct = parseFloat(($elem.text().split('$')[1]))
-            cy.get('.summary_tax_label').then(function ($elem) {
-                const tax = parseFloat(($elem.text().split('$')[1]))
-                cy.log('tax', tax)
-                cy.get('.summary_total_label').then(function ($elem) {
-                    const total = parseFloat(($elem.text().split('$')[1]))
-                    let add = (priceProduct, tax) => parseFloat(priceProduct) + parseFloat(tax);
-                    expect(add(priceProduct, tax)).to.eq(total)
-                })
-            })
-        })
+        cy.validatePrice()
+        cy.addAllProducts()
+        cy.get('.shopping_cart_link').click();
+        cy.get('[data-test="checkout"]').click();
+        cy.get('[data-test="firstName"]').type(loginDates.nameUser);
+        cy.get('[data-test="lastName"]').type(loginDates.lastName);
+        cy.get('[data-test="postalCode"]').type(loginDates.CP);
+        cy.get('[data-test="continue"]').click()
+        cy.scrollDownUp()
+        cy.validatePrice()
     });
 })
